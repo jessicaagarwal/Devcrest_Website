71730ee4b20d2b0ea3065b1d8cc7ccff950782c8
jessicaagarwal <jessica.agarwal.s64@kalvium.community>
2025-01-14 16:40:25 +0530

    Fix routes and update App.jsx for React Router handling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
           <Route path="/project" element={<Projects/>} />
           <Route path="/about" element={<About/>} />
           <Route path="/event" element={<Events/>} />
-{/*           <Route path="*" element={<Navigate to="/" />} /> */}
+          <Route path="*" element={<div>404 - Page Not Found</div>} />
         </Routes>
       </div>
     </Router>
