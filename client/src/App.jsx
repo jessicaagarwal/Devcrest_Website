d34b1471e55a0130b8990011a8965574ff3b2636
jessicaagarwal <jessica.agarwal.s64@kalvium.community>
2025-01-14 18:50:00 +0530

    app.jsx change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,27 @@
 import './App.css';
 import { Navbar } from './components/Navbar';
-// import { Navbar } from './components/Navbar';
 import { Hometext } from './components/Hometext';
 import { Login } from './components/Login';
 import { Signup } from './components/Signup';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Team } from './components/Team';
 import { Projects } from './components/Projects';
 import { About } from './components/About';
 import { Events } from './components/Events';
-// import { Navbar2 } from './components/Navbar2';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // Change to HashRouter
 
 function App() {
   return (
     <Router>
       <div className="bod">
         <Navbar />
-        {/* <Navbar2/> */}
-
         <Routes>
           <Route path="/" element={<Hometext />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup/>} />
-          <Route path="/team" element={<Team/>} />
-          <Route path="/project" element={<Projects/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/event" element={<Events/>} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/project" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/event" element={<Events />} />
           <Route path="*" element={<div>404 - Page Not Found</div>} />
         </Routes>
       </div>
